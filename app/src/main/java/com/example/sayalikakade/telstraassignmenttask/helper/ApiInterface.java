8afef7c62e3fa754e8be516fb6ee7a801991c4c5
KakadeS <sayali.kakade001@gmail.com>
2018-11-14 20:09:30 +0530

    Committed the TestCases and Constant File

--- app/src/main/java/com/example/sayalikakade/telstraassignmenttask/helper/ApiInterface.java
+++ app/src/main/java/com/example/sayalikakade/telstraassignmenttask/helper/ApiInterface.java
@@ -3,11 +3,12 @@ package com.example.sayalikakade.telstraassignmenttask.helper;
 
 
 import com.example.sayalikakade.telstraassignmenttask.model.CountryListModel;
+import com.example.sayalikakade.telstraassignmenttask.utils.CountryConstants;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ApiInterface {
-    @GET("facts.json")
+    @GET(CountryConstants.TARGET_URL)
     Call<CountryListModel> getCountryDetails();
 }
--- app/src/main/java/com/example/sayalikakade/telstraassignmenttask/helper/RetrofitClass.java
+++ app/src/main/java/com/example/sayalikakade/telstraassignmenttask/helper/RetrofitClass.java
@@ -1,15 +1,16 @@
 package com.example.sayalikakade.telstraassignmenttask.helper;
 
+import com.example.sayalikakade.telstraassignmenttask.utils.CountryConstants;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClass {
     private static Retrofit retrofit;
     public static Retrofit getRetrofitObject() {
-        String BASE_URL = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/";
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
+                    .baseUrl(CountryConstants.BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
